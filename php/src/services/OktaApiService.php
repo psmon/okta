59a8ab240aff1f3f28359aa52d053ec956480534
psmon <psmon@live.co.kr>
2019-05-12 19:25:22 +0900

    update api

--- php/src/services/OktaApiService.php
+++ php/src/services/OktaApiService.php
@@ -29,6 +29,7 @@ class OktaApiService
             'client_id' => $this->clientId,
             'redirect_uri' => $this->redirectUri,
             'state' => $state,
+            'scope' => 'openid',
         ]);
         return $url;
     }
